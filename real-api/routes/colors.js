d64c67872e07d6624f38cd24ec32644a2f42069e
Alex Lapinski <contact@alexlapinski.name>
2017-06-08 21:33:46 -0400

    Swapped nodemon for pm2 and added black to test restart on filechange

--- real-api/routes/colors.js
+++ real-api/routes/colors.js
@@ -5,7 +5,8 @@ router.get('/', (req, res) => {
   res.status(200).json([
     { name: 'Red', hex: '#FF0000' },
     { name: 'Green', hex: '#00FF00' },
-    { name: 'Blue', hex: '#0000FF' }
+    { name: 'Blue', hex: '#0000FF' },
+    { name: 'Black', hex: '#000000' }
   ]);
 });
 
